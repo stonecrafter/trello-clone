52a8c8e4acfe234d0c67a44f4c1df8f14b26e248
stonecrafter <kirien.chen@gmail.com>
2018-10-23 07:59:25 -0700

    Updates

---
+++ precache-manifest.cda9f4789248f5573b9d6fe384bc0085.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da88e857b0c45586b2a3",
+    "url": "/trello-clone/static/js/runtime~main.da88e857.js"
+  },
+  {
+    "revision": "8c7338545b5f39853700",
+    "url": "/trello-clone/static/js/main.8c733854.chunk.js"
+  },
+  {
+    "revision": "d05c93e89af97825e7f2",
+    "url": "/trello-clone/static/js/1.d05c93e8.chunk.js"
+  },
+  {
+    "revision": "8c7338545b5f39853700",
+    "url": "/trello-clone/static/css/main.e44d194e.chunk.css"
+  },
+  {
+    "revision": "41757e2178a56a6643d00800cbddfd48",
+    "url": "/trello-clone/index.html"
+  }
+];
